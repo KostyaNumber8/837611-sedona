ae354e75171fc2649defa5aa85a4d23f65f3b941
Овчинников Константин <42147903+KostyaNumber8@users.noreply.github.com>
2018-09-09 15:20:14 +0600

    Исправил ошибки после финальной проврки №1

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 var link = document.querySelector(".main-search");
+var popup = document.querySelector(".appointment-form");
+var form = document.querySelector("form");
+var arrivalDate = document.querySelector(".date-arrival");
+var departureDate = document.querySelector(".date-departure");
 
-      var popup = document.querySelector(".appointment-form");
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("form-show");
+  arrivalDate.focus();
+});
 
-      var form = document.querySelector("form");
-      var arrivalDate = document.querySelector(".date-arrival");
-      var departureDate = document.querySelector(".date-departure");
-
-      link.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        popup.classList.toggle("form-show");
-        arrivalDate.focus();
-      });
-
-      form.addEventListener("submit", function (evt) {
-        if (!arrivalDate.value || !departureDate.value) {
-        evt.preventDefault();
-        popup.classList.remove("form-error");
-        popup.offsetWidth = popup.offsetWidth;
-        popup.classList.add("form-error");
-        console.log("Необходимо выбрать дату заезда и дату выезда");
-        }
-      });
+form.addEventListener("submit", function (evt) {
+  if (!arrivalDate.value || !departureDate.value) {
+    evt.preventDefault();
+    popup.classList.remove("form-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("form-error");
+    console.log("Необходимо выбрать дату заезда и дату выезда");
+  }
+});
